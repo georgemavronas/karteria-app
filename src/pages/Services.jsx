21a4b174331b2dd02364d106ae256b485fea6654
pantoine31 <antonhspap@icloud.com>
2026-04-16 21:40:49 +0300

    service template

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,55 @@
-import "../styles/Services.css";
+import React from 'react';
+import '../styles/Services.css';
 
 function Services() {
   return (
-    <div>
-      <main className="servicePage">
-      </main>
-    </div>
+    <section className="services-section">
+
+      <div className="services-grid">
+
+        <div className="services-card">
+          <h3>Service 1</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+        <div className="services-card">
+          <h3>Service 2</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+        <div className="services-card">
+          <h3>Service 3</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+        <div className="services-card">
+          <h3>Service 4</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+        <div className="services-card">
+          <h3>Service 5</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+        <div className="services-card services">
+          <h3>Service 6</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+        <div className="services-card">
+          <h3>Service 7</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+        <div className="services-card">
+          <h3>Service 8</h3>
+          <p>Description HEREEEE</p>
+        </div>
+
+      </div>
+
+    </section>
   );
 }
 
